3ea31bbf293eab0ce7b87b2e14314d638a1e2956
Joshua J. Williams <116985621+joshjwilli@users.noreply.github.com>
2024-02-01 13:38:52 -0600

    Up to task 2.5

--- assignment0.js
+++ assignment0.js
@@ -1,19 +1,24 @@
-
-let GL, vao, program1, program2;
-let currColor = [0, 0, 0, 0];
-let currTriangles = 1;
-let maxTriangles = 1;
-let useJSON = false;
+let gl, vao, program1, program2, currColor, currTriangles, maxTriangles, useJSON;
 
 window.updateTriangles = function() {
+    currTri = parseInt(document.querySelector("#triangles").value);
 }
 
 window.updateColor = function() {
-
+    var r = parseInt(document.querySelector("#sliderR").value)/255.0;
+    var g = parseInt(document.querySelector("#sliderG").value)/255.0;
+    var b = parseInt(document.querySelector("#sliderB").value)/255.0;
+    currColor = [r, g, b, 1.0];
+    gl.clearColor(currColor[0], currColor[1], currColor[2], currColor[3]);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
 window.checkBox = function() {
-    
+    if (useJSON == false) {
+        useJSON = true;
+    } else {
+        useJSON = false;
+    }
 }
 
 function uploadFile(event) {
@@ -44,8 +49,16 @@ function draw() {
 };
 
 async function initialize() {
-
+    // Setup global vars
+    currColor = [0, 0, 0, 1.0];
+    currTriangles = 1;
+    maxTriangles = 1;
+    useJSON = false;
     // initialive canvas
+    var canvas = document.querySelector("#canvas");
+    gl = canvas.getContext("webgl");
+    gl.clearColor(currColor[0], currColor[1], currColor[2], currColor[3]);
+    gl.clear(gl.COLOR_BUFFER_BIT);
     // create programs
     // draw
 };
